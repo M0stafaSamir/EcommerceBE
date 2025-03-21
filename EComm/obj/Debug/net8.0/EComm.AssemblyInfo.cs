91c4583a3df61b4865b3d34891eff63e17fc2b93
Mostafa Samir <127770642+M0stafaSamir@users.noreply.github.com>
2025-03-21 02:18:10 +0200

    added folders

--- EComm/obj/Debug/net8.0/EComm.AssemblyInfo.cs
+++ EComm/obj/Debug/net8.0/EComm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EComm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb78f1f40b147fbe9be26d7fd924bba24ccc9f8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77406758ca2a52cf27e88408a51bf753dfae405c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EComm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EComm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
