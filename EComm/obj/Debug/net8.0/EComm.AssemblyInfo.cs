af635342948d58bb64ba782a4be2044f48cb2aea
Mostafa Samir <127770642+M0stafaSamir@users.noreply.github.com>
2025-03-21 02:20:01 +0200

    .

--- EComm/obj/Debug/net8.0/EComm.AssemblyInfo.cs
+++ EComm/obj/Debug/net8.0/EComm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EComm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77406758ca2a52cf27e88408a51bf753dfae405c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c4583a3df61b4865b3d34891eff63e17fc2b93")]
 [assembly: System.Reflection.AssemblyProductAttribute("EComm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EComm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
