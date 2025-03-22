f10d2ba666e0ecf647e3a04d43461f667ca653d2
Habosa162 <habosa2000@hotmail.com>
2025-03-22 07:03:35 +0200

    ReviewRepository

---
+++ EComm/Infrastructure/Interfaces/IReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using EComm.Domain.Models;
+
+namespace EComm.Infrastructure.Interfaces
+{
+    public interface IReviewRepository
+    {
+        public Task<Review?> GetReview(int id); 
+        public Task<IEnumerable<Review>> GetProductReviews(int Productid);   
+        public Task SetReview(Review review);   
+        public Task DeleteReview(int id);   
+        public Task UpdateReview(Review review);    
+
+    }
+}
---
+++ EComm/Infrastructure/Repositories/ReviewRepository.cs
@@ -0,0 +1,50 @@
+﻿using EComm.Domain.Models;
+using EComm.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EComm.Infrastructure.Repositories
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Review?> GetReview(int id)
+        {
+            return await _context.Reviews.FirstOrDefaultAsync(r => r.Id == id); 
+        }
+        public async Task DeleteReview(int id)
+        {
+            var review = await GetReview(id);
+            if (review != null) { 
+                _context.Reviews.Remove(review);    
+                await _context.SaveChangesAsync();  
+            }   
+        }
+
+        public async Task<IEnumerable<Review>> GetProductReviews(int Productid)
+        {
+            return await _context.Reviews
+                .Where(r => r.ProductId == Productid)
+                .Include(r => r.Customer)
+                .Include(r => r.Product)
+                .ToListAsync(); 
+        }
+
+        public async Task SetReview(Review review)
+        {
+            await _context.Reviews.AddAsync(review);
+            await _context.SaveChangesAsync(); 
+        }
+
+        public async Task UpdateReview(Review review)
+        {
+            _context.Reviews.Update(review);
+            await _context.SaveChangesAsync(); 
+        }
+    }
+}
