f30a49082f6aa997a1ee8ad89fd347862b86ccce
Habosa162 <habosa2000@hotmail.com>
2025-03-22 04:02:39 +0200

    adding main controllers

---
+++ EComm/API/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using EComm.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EComm.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        //GET /api/products
+        //GET /api/products/{id}
+        //GET /api/categories
+        //POST /api/reviews
+        //GET /api/reviews/{productId}
+
+
+
+
+
+
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- EComm/obj/Debug/net8.0/EComm.AssemblyInfo.cs
+++ EComm/obj/Debug/net8.0/EComm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EComm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448aa1ba7e32a81b4571e85e8d1c55579406d959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f2fef03a46f974ac8bfcbd38e032df0a5a82da7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EComm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EComm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
